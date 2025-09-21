1d5a88463f91f97ea77aaeb3caabc70b8fd10adf
Nerius123 <nerijus.ponomariovas@mif.stud.vu.lt>
2025-09-21 14:53:59 +0300

    Pridetos pradines funkcijos kurios formuoja ribas

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Welcome to Blockchain")
+import sys
+from typing import List
+
+# MASK64: kad skaiciai liktu 64 bitu ribose
+MASK64 = (1 << 64) - 1
+
+def rotl(x: int, r: int) -> int:
+    """Pasukimas i kaire per r bitus (64 bitu ribose)"""
+    return ((x << r) | (x >> (64 - r))) & MASK64
+
+def rotr(x: int, r: int) -> int:
+    """Pasukimas i desine per r bitus (64 bitu ribose)"""
+    return ((x >> r) | (x << (64 - r))) & MASK64
+
