9ec49a81661396dde40bec2d2913cc9648a101d5
Nerius123 <nerijus.ponomariovas@mif.stud.vu.lt>
2025-09-21 14:56:23 +0300

    Papildyta funkcija pirmoji

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ def rotr(x: int, r: int) -> int:
     """Pasukimas i desine per r bitus (64 bitu ribose)"""
     return ((x >> r) | (x << (64 - r))) & MASK64
 
+def u64_le(b: bytes) -> int:
+    """Is 8 baitu suformuoja, kad maziausias baitas eina pirmas"""
+    return int.from_bytes(b, "little")  # perskaito duomenis kaip 64 bitu skaiciu
+
+def to_le8(x: int) -> bytes:
+    """Padaro 8 baitus"""
+    return x.to_bytes(8, "little")  # pavercia i 8 baitus spausdinimui
+
